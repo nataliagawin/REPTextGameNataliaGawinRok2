760073cb999fb48b4518abf40886d7ddd529b04b
filipbrandt <65243602+filipbrandt@users.noreply.github.com>
2023-06-16 12:12:18 +0200

    finished project

--- REPTextGameNataliaGawinRok2/Assets/Scripts/ABC.cs
+++ REPTextGameNataliaGawinRok2/Assets/Scripts/ABC.cs
@@ -5,32 +5,43 @@ using UnityEngine;
 public class ABC : MonoBehaviour
 {
 
+    int min = 1;
+    int max = 1001;
+    int shoot = 500;
+
 
     // Start is called before the first frame update
     void Start()
-    {       
+    {
         Debug.Log("Witam w grze - pomysl w glowie liczbe od 1 do 1000");
         Debug.Log("Czy twoja liczba jest równa/wieksza/mniejsza od 500?");
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-        { 
-            Debug.Log("Nacisnieto gorna strzalke");
+        {
+            min = shoot;
+            NextShoot();
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Nacisnieto dolna strzalke");
+            max = shoot;
+            NextShoot();
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Nacisnieto Enter");
+            Debug.Log("Komputer zgad³ liczbe");
         }
 
+    }
+
+    void NextShoot()
+    {
+        shoot = (min + max) / 2;
+        Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od " + shoot + " ?");
     }
 }
